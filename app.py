6ccd663d65278b788b2284ee522295420e926513
DarekKarasiewicz <dar.karasiewicz@gmail.com>
2021-06-08 14:53:20 +0200

    in post you can clik in name and go to profil (not finished yet)

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import datetime
 from flask import(
     Flask, render_template,request,session,redirect,url_for,)
 
-
 class User:
     def __init__(self,id,username,password):
         self.id = id
@@ -11,12 +10,27 @@ class User:
     def __repr__(self):
         return f'<User:{self.username}'
 
+class Post:
+    def __init__(self,username,text):
+        self.username=username
+        self.text=text
+    def __repr__(self):
+        rep=self.username+ ":"+ self.text
+        return rep
+
+
 users=[]
 users.append(User(id=1,username='Darek',password='123'))
 users.append(User(id=2,username='Krystian',password='123'))
 users.append(User(id=3,username='Bartek',password='pass'))
 users.append(User(id=4, username='Seba',password='123'))
 users.append(User (id=5,username='Marek',password='123'))
+
+posts=[]
+posts.append(Post("Darek","tralalala"))
+posts.append(Post("Darek","tralalala22222"))
+posts.append(Post("Seba","alamakota"))
+posts.append(Post("Bartek","kot nie ma ali"))
 
 app = Flask(__name__)
 app.secret_key='sekretnyklucz'
@@ -42,15 +56,6 @@ def login():
 
     return render_template('login.html')
 
-# def inactivity_logout(fn):
-#     def wrapped(*args,**kwargs):
-#         age = (datetime.datetime.now() - session['timestamp'])
-#         if age > datetime.timedelta(seconds=10):
-#             return redirect(url_for('logout'))
-#         session['timestamp'] = datetime.datetime.now()
-#         return fn(*args, **kwargs)
-#     wrapped.__name__=fn.__name__
-#     return wrapped
 
 @app.route("/logout",methods=('POST',))
 def logout():
@@ -60,12 +65,17 @@ def logout():
 
 @app.route("/profil" ,methods=('GET', 'POST'))
 # @inactivity_logout
-def home():
+def profil():
     # age=session['timestamp']
     # #age=(datetime.datetime.now()-session['timestamp'])
     return render_template('profil.html', name=session['username'])
 @app.route("/main", methods=('GET','Post'))
 def main_side():
+    username=users[0].username
     if request.method=="POST":
-        return redirect(url_for('home'))
-    return render_template('main_side.html')
+        return redirect(url_for('profil'))
+    return render_template('main_side.html',name=username,username=posts[0].username,username1=posts[1].username,
+                           username2=posts[2].username,
+                           username3=posts[3].username,
+                           wpis=posts[0].text,wpis1=posts[1].text,wpis2=posts[2].text,wpis3=posts[3].text)
+
