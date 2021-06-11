aa805b46838c64aec3c7739ff3b142f84c8f2db4
DarekKarasiewicz <dar.karasiewicz@gmail.com>
2021-06-11 14:29:38 +0200

    Adding a database and using it

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import datetime
+import sqlite3
+
 from flask import(
-    Flask, render_template,request,session,redirect,url_for,)
+    Flask, render_template,request,session,redirect,url_for,g)
 
 class User:
     def __init__(self,id,username,password,followers=()):
@@ -19,23 +21,31 @@ class Post:
         rep=self.username+ ":"+ self.text
         return rep
 
-
-users=[]
-users.append(User(id=1,username='Darek',password='123',followers=("Bartek","Seba")))
-users.append(User(id=2,username='Krystian',password='123'))
-users.append(User(id=3,username='Bartek',password='pass'))
-users.append(User(id=4, username='Seba',password='123',followers=("Bartek","Darek")))
-users.append(User (id=5,username='Marek',password='123'))
-
-
-posts=[]
-posts.append(Post(users[0].username,"tralalala"))
-posts.append(Post(users[0].username,"tralalala22222"))
-posts.append(Post(users[4].username,"alamakota"))
-posts.append(Post(users[3].username,"kot nie ma ali"))
-
 app = Flask(__name__)
 app.secret_key='sekretnyklucz'
+
+DATABASE="C:/Users/darek/PycharmProjects/Projekt_v2/twitter.sqlite"
+
+def get_db():
+    db = getattr(g,'_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
 
 @app.route("/")
 def jakos():
@@ -47,10 +57,10 @@ def login():
         session.pop('user_id', None)
         username = request.form['username']
         password = request.form['password']
-        user = [x for x in users if x.username == username]
-        if user and user[0].password == password:
-            session['user_id'] = user[0].id
-            session['username'] = username
+        user = query_db("Select user_id,user_name,password From user Where user_name=?",(username,))
+        if user and user[0]['password'] == password:
+            session['user_id'] = user[0]['user_id']
+            session['username'] = user[0]['user_name']
             session['timestamp']=datetime.datetime.now()
             return redirect(url_for('main_side'))
         else:
@@ -65,26 +75,28 @@ def logout():
     del session['timestamp']
     return redirect(url_for('login'))
 
-@app.route("/profil/<username>" ,methods=('GET', 'POST'))
-def profil(username):
-    for each in users:
-        if each.username==username:
-            user=each
-            break
-    return render_template('profil.html',user=user)
+@app.route("/profil/<int:user_id>" ,methods=('GET', 'POST'))
+def profil(user_id):
+    user=query_db("Select * from user where user_id=?",(user_id,),True)
+    followers=query_db("Select * from user_followers where user_id=?",(user_id,))
+    return render_template('profil.html',followers=followers,user=user)
 
 
 @app.route("/post",methods=('POST',))
 def post():
-    username=session['username']
-    post=Post(username,request.form['new_post'])
-    posts.append(post)
+    user_id=session['user_id']
+    text=request.form['new_post']
+    database=get_db()
+    new_tweet=database.execute("Insert into tweets (user_id,tweet_text) VALUES (?,?)",(user_id,text))
+    database.commit()
+    new_tweet.close()
     return redirect(url_for("main_side"))
 
 @app.route("/main", methods=('GET','Post'))
 def main_side():
-    username=users[0].username
+    posts=query_db("Select * from tweets")
     if request.method=="POST":
-        return redirect(url_for('profil',username=username))
-    return render_template('main_side.html',name=username,posts=posts)
+
+        return redirect(url_for('profil'))
+    return render_template('main_side.html',posts=posts,username=session['username'])
 
